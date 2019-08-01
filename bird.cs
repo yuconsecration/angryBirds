c3e9a14dbe32caff4b33da4e66359f5c97b1db24
“Youryuconsecration <1077767895@qq.com>
2019-08-01 10:23:11 +0800

    限制小鸟拖拽的最大距离

--- bird.cs
+++ bird.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class bird : MonoBehaviour {
     private bool isClick = false;
+    public Transform rightPos;//Transform包含了物体的位置，旋转等信息，这里用来申明一个物体rightPos   
+    public float maxDis = 3;//设置小鸟拖拽的最大距离
     private void OnMouseDown()//当用户在GUIElement或者碰撞器中按下鼠标时系统会自动调用的函数
     {
         isClick = true;
@@ -16,13 +18,18 @@ public class bird : MonoBehaviour {
     {
         if (isClick)//当鼠标一直按下时
         {
-            //transform.position = Input.mousePosition;//将鼠标的位置赋值给当前物体的位置
+            //transform.position = Input.mousePosition;//将鼠标的位置赋值给当前物体的位置，transform理解为当前物体
             //使用上一行代码会存在一个问题，鼠标的位置和当前物体的位置不在同一个坐标系下，小鸟的位置是在世界坐标系下(例如在unity中默认的(0,0)点位置和鼠标所在(0,0)点的位置(鼠标的(0,0)点是在屏幕的左下角)不同)需要来统一坐标系
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);//将屏幕坐标转化为世界坐标
             //这里存在一个问题，拖拽小鸟时出现小鸟的z轴被强制赋值为-10，和相机的z轴保持一致，在3D视角下可以发现，当小鸟的z轴小于-10或者大于0则相机都无法拍摄到小鸟的位置，所以需要强制锁定小鸟拖拽后的z轴的值在-10到0之间
-            //transform.position += new Vector3(0, 0, 6);//强制锁定小鸟的z轴值为-4（方法一）
-	    transform.position += new Vector3(0, 0, -Camera.main.transform.position.z);//将小鸟的z轴坐标加上主摄像机的z轴坐标的赋值来强制锁定小鸟的z轴为0（方法二）
+            //transform.position += new Vector3(0, 0, 6);//强制锁定小鸟的z轴值为-4（方法一），Vector3表示一个三维向量，包含位置，方向等信息
+            transform.position += new Vector3(0, 0, -Camera.main.transform.position.z);//将小鸟的z轴坐标加上主摄像机的z轴坐标的赋值来强制锁定小鸟的z轴为0（方法二）
+            if (Vector3.Distance(transform.position, rightPos.position) > maxDis)//Vector3中有一个方法Distance用来计算两个向量的位移，这里用来计算小鸟和rightPos之间的位移
+            {
+                Vector3 pos = (transform.position - rightPos.position).normalized;//单位化向量，normalized属性用来实现单位化向量
+                pos *= maxDis;//得到最大长度向量
+                transform.position = pos + rightPos.position;//小鸟的位置限制
+            }
         }
     }
 }
-
