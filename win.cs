70310e5a3345e37aa64f476fb4c1605cd04fa6da
“Youryuconsecration <1077767895@qq.com>
2019-08-08 18:02:06 +0800

    win事件函数

---
+++ win.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class win : MonoBehaviour {
+    /// <summary>
+    /// 动画播放完毕后显示星星的操作
+    /// </summary>
+	public void show()
+    {
+        gameManager._instance.showStars();
+    }
+}
