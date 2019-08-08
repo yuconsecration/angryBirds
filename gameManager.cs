a2a07ee796dad75d230a27cd3da622cdb7e4fb5f
“Youryuconsecration <1077767895@qq.com>
2019-08-08 09:46:39 +0800

    解决小鸟轮换速度过大的问题

--- gameManager.cs
+++ gameManager.cs
@@ -7,9 +7,14 @@ public class gameManager : MonoBehaviour
     public List<bird> birds;//创建鸟的集合
     public List<pig> pigs;//创建猪的集合
     public static gameManager _instance;//用来为其他脚本访问该脚本提供接口
+    private Vector3 originPos;//初始化的位置
     private void Awake()
     {
         _instance = this;
+        if(birds.Count > 0) {
+            originPos = birds[0].transform.position;
+        }
+        
     }
     private void Start()
     {
@@ -21,10 +26,11 @@ public class gameManager : MonoBehaviour
     private void Initialized()
     {
         //遍历鸟的集合
-        for (int i = 0; i < birds.Count; i++)
+        for (int i = 0; i < birds.Count; i++)//由于每一只小鸟释放后在集合中都被移除了故下一只小鸟的下标依然为0
         {
             if (i == 0)//第一只小鸟
             {
+                birds[i].transform.position = originPos;//将第一支小鸟的位置赋值给后面每一只小鸟的位置
                 birds[i].enabled = true;
                 //要想访问sp组件，必须保证bird类的sp组件的访问权限为public
                 birds[i].sp.enabled = true;//启用组件
